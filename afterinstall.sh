8251f0b60a25f91811784a5e59aa98c0ee69a773
azeemmd150 <azeem.md03@gmail.com>
2022-11-15 15:27:36 +0530

    bash

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd /home/ubuntu
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 npm -f install
