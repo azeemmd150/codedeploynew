71e0581817207f8f226c2cdf9d196f493995092f
azeemmd150 <azeem.md03@gmail.com>
2022-11-16 13:10:29 +0530

    applicationstart

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd /home/ubuntu
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install v14.21.1
 /home/ubuntu/.nvm/versions/node/v14.21.1/bin/pm2 start -f server.js
